823c1735bea6f4f9dddb7cdc0d41c377c66d6611
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2018-06-14 10:38:00 +0300

    Changeset to samples

---
+++ image13/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  $("#demo img").load(function() {
+    $(this).wrap(function(){
+      return '<span class="image-wrap ' + $(this).attr('class') + '" style="position:relative; display:inline-block; background:url(' + $(this).attr('src') + ') no-repeat center center; width: ' + $(this).width() + 'px; height: ' + $(this).height() + 'px;" />';
+    });
+    $(this).css("opacity","0");
+  });
+
+});
