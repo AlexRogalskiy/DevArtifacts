56766deeb37c263c99ec115bd84a4809e0b4d3fc
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2018-08-04 18:10:23 +0300

    Changeset to samples

---
+++ master/_select-master/README.md
@@ -0,0 +1,66 @@
+_select()
+=======
+284 bytes gzipped<a href="https://raw.github.com/TimPietrusky/_select/master/js/_select.min.js" target="_blank">
+<button>_select.min.js</button>
+</a>
+
+<a href="http://timpietrusky.com/_select" target="_blank" class="alpha">
+timpietrusky.com/_select
+</a>
+
+<section>
+<h2>What?</h2>
+<p>
+  This function extends the native JavaScript <code>select()</code> (which only works on <code>input</code> and <code>textarea</code>) so that it can be used to select the content of every DOM element you want. 
+</p>
+<p>
+  It's written in Vanilla JavaScript (284 bytes gzipped), so you can use it in combination with or without any library. 
+</p>
+
+<b>Works with</b>
+
+<ul>
+  <li>IE 6+</li>
+  <li>any other browser</li>
+</ul>
+
+<b>Forced Focus</b>
+
+<p>
+  <ul>
+    <li>Aggressive focus for <code>input</code> / <code>textarea</code>:<br>
+      Selects the whole content of a text element with one click or touch.</li>
+  </ul>
+</p>
+</section>
+
+<section>
+<h2>How to</h2>
+
+<p>Download <code>_select.min.js</code> and include it into your page / script. </p>
+
+<b>Vanilla JavaScript</b>
+
+	var el = document.querySelector('.demo');
+
+	el.addEventListener('click', function() {
+	  _select(this);
+	});
+        
+<b>jQuery</b>
+
+	var el = $('.demo');    
+
+	el.on('click', function() {
+	  _select(this);
+	});
+	
+</section>
+      
+<div class="pure-g">
+  <div class="pure-u-1">
+    <section class="alpha center">
+      <p>2013 by <a href="http://timpietrusky.com" target="_blank" class="alpha">Tim Pietrusky</a></p>
+    </section>
+  </div>
+</div>
---
+++ master/_select-master/js/main.js
@@ -0,0 +1,110 @@
+/*
+ * _select()
+ *  
+ * Extends the native JavaScript select() (which only works 
+ * on input and textarea) so that it can be used to select 
+ * the content of every DOM element you want.
+ *
+ * _select(document.querySelector('.class')) or 
+ * _select($('.class'))
+ *
+ * timpietrusky.com/_select
+ * 
+ * 
+ * 2013 by Tim Pietrusky
+ * timpietrusky.com
+ */
+function _select(el) {
+  var d = document, r, s;
+  
+  // Transform jQuery into vanilla JS
+  el = typeof el[0] === 'undefined' ? el : el[0];
+  
+  try {
+    // Try the default select() for input / textarea
+    el.select();
+
+    // Try to disable mouseup on input / textarea
+    // in order to don't lose focus on some (mobile) devices 
+    // -> forced focus!
+    try {
+      if (typeof(el.dataset.mouseup) === 'undefined') {
+        el.dataset.mouseup = 'true';
+        
+        // Disable mouseup on intput / textarea
+        el.addEventListener('mouseup', function(e) {
+          e.preventDefault();
+        }, false); 
+      }
+    } catch(e) {}
+  } catch(e) {
+    
+    // Fallback for most browsers
+    if (window.getSelection) {
+      s = window.getSelection();
+      r = d.createRange();
+      r.selectNodeContents(el);
+      s.removeAllRanges();
+      s.addRange(r);
+      
+    // Fallback for <= IE8
+    } else if (d.body.createTextRange) { 
+      r = d.body.createTextRange();
+      r.moveToElementText(el);
+      r.select();
+    }
+  }
+}
+
+
+
+
+
+/**
+ *
+ * Demo code
+ *
+ */
+var d = document;
+
+/**
+ * Helper function for event binding. 
+ */ 
+function bind(el, eventName, eventHandler) {
+  var el = document.getElementsByClassName(el)[0];
+  
+  if (el.addEventListener) {
+    el.addEventListener(eventName, eventHandler, false); 
+  } else if (el.attachEvent){
+    el.attachEvent('on'+eventName, eventHandler);
+  }
+}
+
+/**
+ *
+ * Bind click events for all the 5 demos.
+ *
+ */ 
+bind('demo1', 'click', function () {
+  _select(this);
+});
+
+bind('demo2', 'click', function () {
+  _select(this.children[0]);
+});
+
+bind('demo3', 'click', function () {
+  _select(this);
+});
+
+bind('demo4', 'click', function () {
+  _select(this);
+});
+
+bind('demo5', 'click', function () {
+  _select(this);
+});
+
+bind('demo6--button', 'click', function () {
+  _select(document.getElementsByClassName('demo6')[0]);
+});
