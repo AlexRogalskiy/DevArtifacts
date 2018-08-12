1331a328a8f0c962f0d1b6f8cf0ddb76a395e07b
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2018-08-12 18:07:29 +0300

    Changeset to samples

---
+++ menu-master/menu60/js/script.js
@@ -0,0 +1,10 @@
+$('.block-menu').find('a').each(function(){
+
+  var el = $(this),
+       elText = el.text();
+  
+  el.addClass("three-d");
+  el.append('<span aria-hidden="true" class="three-d-box"><span class="front">'+elText+'</span><span class="back">'+elText+'</span></span>');
+
+
+});
