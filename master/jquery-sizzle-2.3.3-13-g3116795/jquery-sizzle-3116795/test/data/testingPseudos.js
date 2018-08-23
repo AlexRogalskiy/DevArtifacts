dd4bb2f72c6093bcb0efb213c935b794fe418543
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2018-08-23 23:21:36 +0300

    Changeset to samples

--- master/jquery-sizzle-2.3.3-13-g3116795/jquery-sizzle-3116795/test/data/testingPseudos.js
+++
@@ -1,4 +0,0 @@
-// A custom pass-through pseudo to force non-qSA processing
-Sizzle.selectors.filters.noQSA = function( elem ) {
-	return true;
-};
--- master/madebysource-lesshat-4.1.0-0-g4dda903/madebysource-lesshat-8f72496/tasks/src/test-env.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-var suite = require('./test-suite');
-
-var tester = new suite();
-
-var buildPath = path.resolve(__dirname, '..', '..', 'build', 'lesshat.less');
-tester.addBuildResult(buildPath);
-
-global.test = tester;
