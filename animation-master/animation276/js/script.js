1d498cfef2663beb6bb912833f4e3d97d7810b01
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2018-08-25 17:41:23 +0300

    Changeset to samples

---
+++ animation-master/animation276/js/script.js
@@ -0,0 +1,22 @@
+/*
+ * Monster Energy SVG logo animated
+ *
+ * A simple animation for the monsterenergy.com logo.
+ * Why? I love Monster Energy drinks. 
+ * Just :hover to reset the animation. 
+ *
+ * 
+ * You can support this with a vote:
+ *
+ * Reddit
+ http://www.reddit.com/r/web_design/comments/1hxcw1/monster_energy_logo_as_svg_and_animated_with_css/
+ *
+ * Hacker News
+ * https://news.ycombinator.com/item?id=6012419
+ *
+ * This is a fan remake. Please don't sue me!
+ *
+ * 
+ * 2013 by Tim Pietrusky
+ * timpietrusky.com
+ */
